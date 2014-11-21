8c7f0e44dac245be9ee969d0860f08350ec7533f
KITE <ixkaito@gmail.com>
2014-11-21 12:06:31 +0900

    Initial commit

---
+++ jquery.plugin.js
@@ -0,0 +1,20 @@
+;(function($, undefined){
+
+  "use strict";
+
+  $.fn.pluginname = function(options) {
+
+    var defaults = {
+      opt1 : 'foo',
+      opt2 : 'bar'
+    };
+
+    var settings = $.extend(true, {}, defaults, options);
+
+    return this.each(function() {
+      // Do something awesome.
+    });
+
+  };
+
+})(jQuery);
